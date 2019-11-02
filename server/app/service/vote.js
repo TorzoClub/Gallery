90c47818985adf105c5692544340863d9058c518
vec <vec@torzo.club>
2019-11-03 02:51:58 +0800

    [adjust] 创建投票加上事务和锁

--- server/app/service/vote.js
+++ server/app/service/vote.js
@@ -21,47 +21,55 @@ module.exports = app =>
     }
 
     async create({ gallery_id, photo_id, qq_num }) {
-      const gallery = await this.GalleryModel.findByPk(gallery_id);
-      if (!gallery) {
-        throw new app.WarningError('相册不存在', 404);
-      }
-
-      const photo = await this.PhotoModel.findByPk(photo_id);
-      if (!photo) {
-        throw new app.WarningError('照片不存在', 404);
-      }
-
-      if (photo.gallery_id !== gallery.id) {
-        throw new app.WarningError('相册和照片不匹配', 404);
-      }
-
-      const member = await this.MemberModel.findOne({
-        where: { qq_num },
-      });
-      if (!member) {
-        throw new app.WarningError('成员不存在', 404);
-      }
-
-      const vote = await this.VoteModel.findOne({
-        where: { gallery_id, photo_id, member_id: member.id },
-      });
-
-      if (vote) {
-        throw new app.WarningError('已经投过这个相片了', 409);
-      }
-
-      if (gallery.vote_limit > 0) {
-        if (photo.vote_count > gallery.vote_limit) {
-          throw new app.WarningError('票数限制', 403);
+      return this.app.model.transaction(async transaction => {
+        const gallery = await this.GalleryModel.findByPk(gallery_id, { transaction, lock: transaction.LOCK.UPDATE });
+        if (!gallery) {
+          throw new app.WarningError('相册不存在', 404);
         }
-      }
 
-      photo.increment('vote_count', { by: 1 });
+        const photo = await this.PhotoModel.findByPk(photo_id, { transaction, lock: transaction.LOCK.UPDATE });
+        if (!photo) {
+          throw new app.WarningError('照片不存在', 404);
+        }
 
-      return await this.VoteModel.create({
-        gallery_id,
-        photo_id,
-        member_id: member.id,
+        if (photo.gallery_id !== gallery.id) {
+          throw new app.WarningError('相册和照片不匹配', 404);
+        }
+
+        const member = await this.MemberModel.findOne({
+          where: { qq_num },
+
+          transaction,
+          lock: transaction.LOCK.UPDATE,
+        });
+        if (!member) {
+          throw new app.WarningError('成员不存在', 404);
+        }
+
+        const vote = await this.VoteModel.findOne({
+          where: { gallery_id, photo_id, member_id: member.id },
+
+          transaction,
+          lock: transaction.LOCK.UPDATE,
+        });
+
+        if (vote) {
+          throw new app.WarningError('已经投过这个相片了', 409);
+        }
+
+        if (gallery.vote_limit > 0) {
+          if (photo.vote_count > gallery.vote_limit) {
+            throw new app.WarningError('票数限制', 403);
+          }
+        }
+
+        await photo.increment('vote_count', { by: 1, transaction, lock: transaction.LOCK.UPDATE });
+
+        return await this.VoteModel.create({
+          gallery_id,
+          photo_id,
+          member_id: member.id,
+        }, { transaction });
       });
     }
   };

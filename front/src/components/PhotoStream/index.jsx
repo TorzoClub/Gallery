0c61944d0af1fbe9af105e704765dbc57095e033
vec <vec@torzo.club>
2020-11-23 23:31:31 +0800

    [improve] tranform function Component

--- front/src/components/PhotoStream/index.jsx
+++ front/src/components/PhotoStream/index.jsx
@@ -6,60 +6,52 @@ import PhotoBox from 'components/PhotoBox'
 
 const SAME_HEIGHT = 100
 
-export default class PhotoStream extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-    }
+const witchHeightIsMinimum = columns =>
+  columns.indexOf(Math.min(...columns))
+
+const computeColumnHeight = column => 
+  column
+    .map(photo => SAME_HEIGHT * (photo.height / photo.width))
+    .reduce((a, b) => a + b, 0)
+
+const createColumns = (column_count, photos) => {
+  const columns = Array.from(Array(column_count)).map(() => [])
+
+  photos.forEach(photo => {
+    // console.log('photo', photo)
+    const columnsHeight = columns.map(computeColumnHeight)
+
+    const min_height_index = witchHeightIsMinimum(columnsHeight)
+    columns[min_height_index].push(photo)
+  })
+
+  return columns
+}
+
+export default (props) => {
+  const { screen, column_count, photos, total_width, gutter = '0px' } = props
+
+  const columns = createColumns(column_count, photos)
+
+  const isMobile = screen === 'mobile'
+
+  let photoStreamListWidth
+  if (isMobile) {
+    photoStreamListWidth = `calc(100vw - ${gutter} * ${column_count} + (${gutter} / 2))`
+  } else {
+    photoStreamListWidth = `calc(${total_width} + ${gutter} * ${column_count - 1})`
   }
 
-  witchHeightIsMinimum = columns =>
-    columns.indexOf(Math.min(...columns))
-
-  computeColumnHeight = column =>
-    column.length && column
-      .map(photo => SAME_HEIGHT * (photo.height / photo.width))
-      .reduce((a, b) => a + b)
-
-  createColumns = () => {
-    const columns = Array.from(
-      Array(this.props.column_count)
-    ).map(() => [])
-
-    this.props.photos.forEach(photo => {
-      const columnsHeight = columns.map(this.computeColumnHeight)
-
-      const min_height_index = this.witchHeightIsMinimum(columnsHeight)
-      columns[min_height_index].push(photo)
-    })
-
-    return columns
+  let boxWidth
+  if (isMobile) {
+    boxWidth = `(${total_width} / ${column_count} - ${gutter})`
+  } else {
+    boxWidth = `(${total_width} / ${column_count})`
   }
 
-  render() {
-    const { screen, column_count, total_width } = this.props
-    const { gutter = '0px' } = this.props
-
-    const columns = this.createColumns()
-
-    const isMobile = screen === 'mobile'
-
-    let photoStreamListWidth
-    if (isMobile) {
-      photoStreamListWidth = `calc(100vw - ${gutter} * ${column_count} + (${gutter} / 2))`
-    } else {
-      photoStreamListWidth = `calc(${total_width} + ${gutter} * ${column_count - 1})`
-    }
-
-    let boxWidth
-    if (isMobile) {
-      boxWidth = `(${total_width} / ${column_count} - ${gutter})`
-    } else {
-      boxWidth = `(${total_width} / ${column_count})`
-    }
-
-    return <div
-      className={`photo-stream ${ screen }`}
+  return (
+    <div
+      className={`photo-stream ${screen}`}
       style={{
         width: photoStreamListWidth
       }}
@@ -68,32 +60,32 @@ export default class PhotoStream extends React.Component {
         columns.map((column, key) => (
           <div
             className="steam-column"
-            key={ String(key) }
-            style={ {
+            key={String(key)}
+            style={{
               width: `calc(${boxWidth})`
               // marginLeft: key ? '' : gutter,
               // marginRight: gutter,
               // paddingLeft: key ? '' : gutter,
               // paddingRight: gutter
-            } }
+            }}
           >
             {
               column.map(photo => (
                 <PhotoBox
-                  gallery={this.props.gallery}
+                  gallery={props.gallery}
                   screen={screen}
                   gutter={gutter}
                   photo={photo}
-                  toDetail={this.props.toDetail}
-                  width={ photo.width }
-                  height={ photo.height }
-                  boxWidth={ boxWidth }
-                  key={ photo.id.toString() } />
+                  toDetail={props.toDetail}
+                  width={photo.width}
+                  height={photo.height}
+                  boxWidth={boxWidth}
+                  key={photo.id.toString()} />
               ))
             }
           </div>
         ))
       }
     </div>
-  }
+  )
 }

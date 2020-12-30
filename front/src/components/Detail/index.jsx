5befa629480ef6062f2e1dfaa45e02085a17d337
vec <vec@torzo.club>
2020-12-30 22:17:49 +0800

    公告弹窗禁用滚动

--- front/src/components/Detail/index.jsx
+++ front/src/components/Detail/index.jsx
@@ -1,5 +1,6 @@
 import vait from 'vait'
 import React, { useRef, useEffect, useState } from 'react'
+import useDisableScroll from 'hooks/useDisableScroll'
 
 import './style.scss'
 
@@ -281,54 +282,7 @@ export default ({
     }
   }, [detail, detailFrameEl.current])
 
-  useEffect(() => {
-    if (!detail) {
-      return
-    }
-
-    var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
-
-    function preventDefault(e) {
-      e.preventDefault();
-    }
-
-    function preventDefaultForScrollKeys(e) {
-      if (keys[e.keyCode]) {
-        preventDefault(e);
-        return false;
-      }
-    }
-
-    // modern Chrome requires { passive: false } when adding event
-    let supportsPassive = false;
-    try {
-      window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-        // eslint-disable-next-line getter-return
-        get() { supportsPassive = true; }
-      }));
-    } catch (e) { }
-
-    const wheelOpt = supportsPassive ? { passive: false } : false;
-    const wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-    function disableScroll() {
-      window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-      window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-      window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-      window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-    }
-
-    function enableScroll() {
-      window.removeEventListener('DOMMouseScroll', preventDefault, false);
-      window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-      window.removeEventListener('touchmove', preventDefault, wheelOpt);
-      window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-    }
-
-    disableScroll()
-
-    return enableScroll
-  }, [detail])
+  useDisableScroll(Boolean(detail))
 
   const handleClickFrame = (e) => {
     onCancel()

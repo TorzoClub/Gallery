e64d4260d71703425390607dabd1ef69c0a5d286
vec <vec@torzo.club>
2023-12-15 14:28:12 +0800

    [add] server refresh

--- scripts/server-refresh.sh
+++ scripts/server-refresh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd ${SERVER_PATH}
-cd server
-nvm use 18.17.0
-npm run stop-dev
-git fetch
-git pull
-npm install
-npm run start-dev
+# cd ${SERVER_PATH}
+# cd server
+# nvm use 18.17.0
+# npm run stop-dev
+# git fetch
+# git pull
+# npm install
+# npm run start-dev

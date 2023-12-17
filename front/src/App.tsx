af9d643442fa7f01f9184b751c3f4b9e48867e5e
vec <vec@torzo.club>
2023-12-17 14:33:12 +0800

    [update] use vait2.6.0

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -13,39 +13,41 @@ const err_sig = Signal<string>()
 export const AppCriticalError = err_sig.trigger
 
 export default function App() {
-  const app_node = (
-    <div className="app">
-      {
-        process.env.REACT_APP_BUILD_DESCRIPTION && process.env.REACT_APP_BUILD_DESCRIPTION.length && (
-          <pre className="build-description">
-            <code>{ process.env.REACT_APP_BUILD_DESCRIPTION }</code>
-          </pre>
-        )
-      }
-
-      <GalleryHome />
-
-      <style>{`
-        .app {
-          background-image: url(${BgImageUrl});
-          background-repeat: repeat;
-        }
-        .build-description {
-          color: grey;
-          font-size: 12px;
-          padding: 0;
-          margin: 0;
-        }
-      `}</style>
-    </div>
-  )
-
-  const [ showFailure, , failure_layout ] = useFailureLayout(app_node)
+  const [ showFailure, , failure_layout ] = useFailureLayout(<AppInner />)
 
   useEffect(() => {
     err_sig.receive(showFailure)
-    return () => err_sig.unReceive(showFailure)
+    return () => err_sig.cancelReceive(showFailure)
   }, [showFailure])
 
   return <>{failure_layout}</>
+}
+
+function AppInner() {
+  return (
+    <div className="app">
+    {
+      process.env.REACT_APP_BUILD_DESCRIPTION && process.env.REACT_APP_BUILD_DESCRIPTION.length && (
+        <pre className="build-description">
+          <code>{ process.env.REACT_APP_BUILD_DESCRIPTION }</code>
+        </pre>
+      )
+    }
+
+    <GalleryHome />
+
+    <style>{`
+      .app {
+        background-image: url(${BgImageUrl});
+        background-repeat: repeat;
+      }
+      .build-description {
+        color: grey;
+        font-size: 12px;
+        padding: 0;
+        margin: 0;
+      }
+    `}</style>
+  </div>
+  )
 }

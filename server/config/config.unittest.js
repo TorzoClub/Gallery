7f3f24deedef3dc736ddb0a9865066515cbfc28a
vec <vec@torzo.club>
2025-11-04 14:01:51 +0800

    update mysql test database root password

--- server/config/config.unittest.js
+++ server/config/config.unittest.js
@@ -68,7 +68,7 @@ module.exports = appInfo => {
     host: '127.0.0.1',
     port: 3306,
     username: 'root',
-    password: 'vechk123',
+    password: '7355608',
 
     charset: 'utf8mb4',
     collate: 'utf8mb4_general_ci',
